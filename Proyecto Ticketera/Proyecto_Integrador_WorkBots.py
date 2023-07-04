8012ad6a20527b799c17ac089aac325ec7acdd34
damianqnz <79475339+damianqnz@users.noreply.github.com>
2023-07-04 02:23:50 +0100

    Update Proyecto_Integrador_WorkBots.py
    ultima actualizacion

--- Proyecto Ticketera/Proyecto_Integrador_WorkBots.py
+++ Proyecto Ticketera/Proyecto_Integrador_WorkBots.py
@@ -367,3 +367,118 @@ def mostrarTicketCred(productos: List[Producto]):
     print("-" * 85)
     print("|%83s|" % ("Total: " + str(total)))
     print("|%83s|" % ("Impuestos: " + str(total - total / 1.21)))
+
+# Función del programa ticketera
+def TicketeraMain():
+    productos = []
+    while True:
+        nombre = input("***¡¡Bienvenidos al supermercado WorkBots!!***\n\nPor favor, escriba su nombre: ")
+        insertar_cliente(nombre)
+        while True:
+            if productos:
+                print("\n" + "-" * 106)
+                print("|{:22s}|{:22s}|{:22s}|{:22s}|".format("Descripción", "Precio", "Cantidad", "Subtotal"))
+                print("-" * 106)
+                for p in productos:
+                    print("|{:22s}|{:22f}|{:22f}|{:22f}|".format(p.descripcion, p.precio, p.cantidad, p.subtotal))
+                print("-" * 106)
+
+            print("\n¡Bienvenido {}!".format(nombre))
+            print("\nElija su opción:")
+            print("1. Agregar producto")
+            print("2. Cambiar cantidad")
+            print("3. Quitar producto")
+            print("4. Mostrar ticket y terminar venta")
+            print("5. Salir")
+
+            eleccion = input("Seleccione: ")
+            if eleccion == "1":
+                agregar_producto(productos)
+            elif eleccion == "2":
+                cambiarCantidad(productos)
+            elif eleccion == "3":
+                quitarProducto(productos)
+            elif eleccion == "4":
+                if len(productos)!=0 :
+                        eleccionPago = input("Por favor, seleccione el método de pago:\n"
+                                     "1. Efectivo\n"
+                                     "2. Tarjeta débito\n"
+                                     "3. Tarjeta crédito\n"
+                                     "4. Volver al menú anterior\n"
+                                     "5. Salir\n"
+                                     "Seleccione: ")
+                        if eleccionPago == "1":
+                            mostrarTicketEfe(productos)
+                            print("Gracias por su compra, {}!!".format(nombre))
+                            break
+                        elif eleccionPago == "2":
+                            mostrarTicketDebit(productos)
+                            print("Gracias por su compra, {}!!".format(nombre))
+                            break
+                        elif eleccionPago == "3":
+                            mostrarTicketCred(productos)
+                            print("Gracias por su compra, {}!!".format(nombre))
+                            break
+                        elif eleccionPago == "4":
+                            print("Volviendo...")
+                        elif eleccionPago == "5":
+                            break
+                        else:
+                            print("Opción no válida!")
+                else:
+                    print('Debe agregar al menos un articulo!')
+
+            elif eleccion == "5":
+                break
+            else:
+                print("Opción no válida!")
+
+        fin = input("¿Nuevo Cliente? ¿Desea continuar?\n"
+                    "1. Si\n"
+                    "2. No\n"
+                    "Seleccione: ")
+        if fin == "1":
+            productos.clear()
+            print("Volviendo...")
+            print("-" * 106)
+            print("-" * 106)
+        elif fin == "2":
+            print("Gracias por visitar el supermercado WorkBots!")
+            break
+        else:
+            print("Opción no válida!")
+
+#Funcion principal de menu para ticketera y base de datos
+def MenuPrincipal():
+    while True:
+        print(" ")
+        print(" ")
+        print("-" * 106)
+        print("***¡¡Bienvenido al sistema de supermercado WorkBots!!*** ")
+        print("\nElija su opción:")
+        print("1. Crear tablas para la base de datos de: productos, clientes y ventas")
+        print("2. Agregar productos a la base de datos")
+        print("3. Ver productos en la base de datos")
+        print("4. Eliminar producto de la base de datos")
+        print("5. Ir al programa de Ticketera")
+        print("6. Salir")
+
+        eleccion = input("Seleccione: ")
+        if eleccion == "1":
+            crear_tablas()
+        elif eleccion == "2":
+            AgregarProdBD()
+        elif eleccion == "3":
+            VerProdBD()
+        elif eleccion == "4":
+            BorrarProdBD()
+        elif eleccion == "5":
+            TicketeraMain()
+        elif eleccion == "6":
+            print("-" * 106)
+            break
+        else:
+            print("Opción no válida!")
+
+if __name__ == "__main__":
+    MenuPrincipal()
